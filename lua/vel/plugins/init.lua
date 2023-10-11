5c31dfb478540493787d1fc586f03d0db353bd02
vel <powervel1995@gmail.com>
2023-10-12 02:08:25 +0530

    :tada: :sparkles: initial commit

---
+++ lua/vel/plugins/init.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"nvim-tree/nvim-web-devicons",
+	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+}
