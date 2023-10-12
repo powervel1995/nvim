ddcce1e84265eee40f2c4e9906e8946e9dac309a
vel <powervel1995@gmail.com>
2023-10-12 20:57:25 +0530

    :tada: :sparkles: initial commit

--- lua/vel/plugins/lsp/none-ls.lua
+++ lua/vel/plugins/lsp/none-ls.lua
@@ -1,7 +1,7 @@
 return {
 	"nvimtools/none-ls.nvim", -- configure formatters & linters
 	lazy = true,
-	event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
+	-- event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
 	config = function()
 		local null_ls = require("null-ls")
 
